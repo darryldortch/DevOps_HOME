c5dee3e82f63b036ff9b2d9be1f0c2e1d893aff2
darryldortch <darryl.dortch@salientfed.com>
2016-04-13 11:10:41 -0400

    New Change

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,7 @@
 public class HelloWorld { 
    public static void main(String[] args) { 
       System.out.println("Hello, World");
-       System.out.println("Dallas Cowboys will win the Super Bowl");
-       System.out.println("Thank You Igor & Pradeep!!!");
+       
    }
 }
 
